6f2ff9d369815b71e98f501c3709165e26506a15
Matěj Svoboda <56925305+Svobyyy@users.noreply.github.com>
2025-02-18 18:05:33 +0000

    Sidebar

--- components/UI/Sidebar/Sidebar.tsx
+++ components/UI/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
+"use client";
 import LogoLarge from "@/components/Icons/LogoLarge";
 import MinimizeIcon from "@/components/Icons/Navbar/MinimizeIcon";
 import Link from "next/link";
 import SidebarOptions from "./SidebarOptions";
+import { useState } from "react";
 // import LogoSmall from "@/components/Icons/LogoSmall";
 
 function Sidebar({ active }: { active: string }) {
+
+  const [minimized, setMinimized] = useState(false);
+
   return (
     <header className="max-w-[300px] w-full h-screen text_preset_3">
       <nav className="fixed top-0 max-w-[300px] w-full bg-grey-900 text-grey-300 flex justify-between flex-col h-screen rounded-r-2xl">
@@ -15,9 +20,11 @@ function Sidebar({ active }: { active: string }) {
           </Link>
           <SidebarOptions active={active} />
         </section>
-        <button className="hover:text-grey-100 transition-colors py-4 capitalize flex gap-4 justify-start items-center cursor-pointer pl-8 mr-6">
+        <button 
+        onClick={() => setMinimized((mini) => !mini)}
+        className="hover:text-grey-100 transition-colors py-4 capitalize flex gap-4 justify-start items-center cursor-pointer pl-8 mr-6 min-h-14">
           <MinimizeIcon />
-          <h3>Minimize</h3>
+          <h3 className={`${minimized ? "hidden" : "inline-block"}`}>Minimize</h3>
         </button>
       </nav>
     </header>
--- components/UI/Sidebar/SidebarOption.tsx
+++ components/UI/Sidebar/SidebarOption.tsx
@@ -10,20 +10,16 @@ function SidebarOption({
   children?: React.ReactNode;
   active: string;
 }) {
+  const titleFormated = title.toLowerCase().replaceAll(" ", "_");
+
   return (
-    <li
-      className={`${
-        active !== title.toLowerCase().replaceAll(" ", "_")
-          ? "hover:text-grey-100"
-          : ""
-      } transition-colors`}
-    >
+    <li>
       <Link
-        href={`${title.toLowerCase().replaceAll(" ", "_")}`}
-        className={`py-4 capitalize flex gap-4 justify-start items-center pl-8 mr-6 ${
-          active === title.toLowerCase().replaceAll(" ", "_")
+        href={`${titleFormated}`}
+        className={`py-4 capitalize flex gap-4 justify-start items-center pl-8 mr-6 relative min-h-14 ${
+          active === titleFormated
             ? "bg-beige-100 rounded-r-xl border-l-4 border-green text-grey-900 pl-[27px]"
-            : ""
+            : "hover:text-grey-100 transition-colors"
         }`}
       >
         {children}
